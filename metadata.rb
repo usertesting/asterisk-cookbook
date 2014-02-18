a13024c19ac577744cb4f06745f03af59e03d8a3
Ben Langfeld <ben@langfeld.me>
2014-02-18 17:21:29 -0300

    Update unimrcp cookbook for CentOS compat

--- metadata.rb
+++ metadata.rb
@@ -11,7 +11,7 @@ recipe 'asterisk::unimrcp', 'Install Asterisk UniMRCP plugin and configure'
 
 depends 'apt', '~> 2.2'
 depends 'build-essential', '~> 1.4'
-depends 'unimrcp', '~> 0.1'
+depends 'unimrcp', '~> 0.2'
 
 supports 'debian', '>= 7.1'
 supports 'ubuntu', '>= 10.04'
